bb0f202cbc1e8a3a15a2909f6a7b4b509e535499
MarkNote <cookft@163.com>
2018-05-23 22:10:00 +0800

    v.1.0.7 18/05/23/noteEdit

---
+++ pyg_manager_web/src/main/java/com/huyaaaaaa/manager/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.huyaaaaaa.manager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("login")
+public class LoginController  {
+    public Map name(){
+
+    }
+}
